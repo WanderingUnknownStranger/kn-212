a44402d873628d7d9927bb2f0ed19b146f5b5da4
knightfireholder <knightfireholder@gmail.com>
2020-06-01 08:08:03 +0300

    adevent

--- script/script.js
+++ script/script.js
@@ -1,12 +1,8 @@
-function openForm() {
-    document.getElementById("login-form").style.display = "block";
-}<div id="login-form" class="lg-form"><form>
-            <span>Name:</span>
-            <span><input type="text" name="username"></span>
-            <span>Age:</span>
-            <span><input type="number" name="userage"></span>
-            <span>Gender:</span>
-            <span><input type="radio" name="choise" value="g1">Man<br>
-            <input type="radio" name="choise" value="g2">Woman</span>
-            <span><input type="submit"></span>
-        </form></div>
+let subbut = document.querySelector(".submit-button");
+subbut.addEventListener('click', getName);
+function getName(){
+    let form = document.forms.reg;
+    let fname = document.forms.name;
+    let fage = document.forms.age;
+    alert(fname); 
+}
