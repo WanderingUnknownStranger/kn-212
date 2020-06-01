740f3ebfd1f6c0a93a37315212a36ecbad827cf8
knightfireholder <knightfireholder@gmail.com>
2020-06-01 09:23:39 +0300

    done (but very-very bad ヽ(*ﾟ д ﾟ)ノ

--- script/script.js
+++ script/script.js
@@ -1,3 +1,12 @@
+document.querySelector('.theme-toggle').onclick = function()
+{
+    if(document.querySelector('.style-link').getAttribute("href") == "css/light.css"){
+        document.querySelector('.style-link').setAttribute("href", "css/dark.css");
+    }
+    else if(document.querySelector('.style-link').getAttribute("href") == "css/dark.css"){
+        document.querySelector('.style-link').setAttribute("href", "css/light.css");
+    }
+};
 let subbut = document.querySelector(".submit-button");
 subbut.addEventListener('click', getInfo);
 function getInfo(){
@@ -12,12 +21,3 @@ function getInfo(){
 let lbl = document.querySelector(".events-label");
 lbl.addEventListener('mouseover', function(){lbl.classList.add('lbl')});
 lbl.addEventListener('mouseout', function(){lbl.classList.remove('lbl')});
-document.querySelector('.theme-toggle').onclick = function()
-{
-    if(document.querySelector('.style-link').getAttribute("href") == "css/light.css"){
-        document.querySelector('.style-link').setAttribute("href", "css/dark.css");
-    }
-    else if(document.querySelector('.style-link').getAttribute("href") == "css/dark.css"){
-        document.querySelector('.style-link').setAttribute("href", "css/light.css");
-    }
-};
