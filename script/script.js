1c4760023d1ca84e207a0c39728ded4c08a907dd
knightfireholder <knightfireholder@gmail.com>
2020-06-01 09:18:26 +0300

    switch dont work

--- script/script.js
+++ script/script.js
@@ -12,3 +12,12 @@ function getInfo(){
 let lbl = document.querySelector(".events-label");
 lbl.addEventListener('mouseover', function(){lbl.classList.add('lbl')});
 lbl.addEventListener('mouseout', function(){lbl.classList.remove('lbl')});
+document.querySelector('.theme-toggle').onclick = function()
+{
+    if(document.querySelector('.style-link').getAttribute("href") == "css/light.css"){
+        document.querySelector('.style-link').setAttribute("href", "css/dark.css");
+    }
+    else if(document.querySelector('.style-link').getAttribute("href") == "css/dark.css"){
+        document.querySelector('.style-link').setAttribute("href", "css/light.css");
+    }
+};
